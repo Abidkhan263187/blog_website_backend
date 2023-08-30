57fb31cdeda0f43e87e0847055f891c69c66dab1
abidkhan263187 <abidkhan263187@gmail.com>
2023-08-31 03:01:30 +0530

    update

--- RouteController/tourism.js
+++ RouteController/tourism.js
@@ -5,10 +5,28 @@ const {tourModel}= require('../models/tourismModel')
 const tourismRouter=Router()
 
 
-tourismRouter.get('/',async(req,res)=>{
- let data= await  tourModel.find()
-console.log(data)
-res.send({mssg:"hi from travel page",data:data})
-})
+
+tourismRouter.get('/', async (req, res) => {
+    const {Continent, CountryName } = req.query; // Get the CountryName query parameter
+    
+    try {
+      let data;
+  
+      if (CountryName && Continent) {
+        console.log(CountryName)
+        data = await tourModel.find({"Continent":Continent,"countries.CountryName":CountryName},{"countries.$":1})
+
+      } else {
+        data = await tourModel.find(req.query);
+      }
+  
+      console.log(data);
+      res.send({ mssg: "Data retrieved successfully", data: data });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: "An error occurred while fetching data" });
+    }
+  });
+  
 
 module.exports={tourismRouter}
